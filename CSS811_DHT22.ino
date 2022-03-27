a773cf1d78d7e251c83c28988af11759c0ad8d76
hitoridekimasu <hitoridekimasu@gmail.com>
2022-03-27 11:55:35 +0200

    completo v1

--- CSS811_DHT22.ino
+++ CSS811_DHT22.ino
@@ -1,21 +1,6 @@
-/***************************************************************************
-  This is a library for the CCS811 air
-
-  This sketch reads the sensor
-
-  Designed specifically to work with the Adafruit CCS811 breakout
-  ----> http://www.adafruit.com/products/3566
-
-  These sensors use I2C to communicate. The device's I2C address is 0x5A
-
-  Adafruit invests time and resources providing this open source code,
-  please support Adafruit andopen-source hardware by purchasing products
-  from Adafruit!
-
-  Written by Dean Miller for Adafruit Industries.
-  BSD license, all text above must be included in any redistribution
- ***************************************************************************/
-
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <InfluxDbClient.h>
 #include "Adafruit_CCS811.h"
 #include "DHT.h" // Libreria para manejar sensor de temperatura
 
@@ -25,26 +10,128 @@
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
 const int DHTPin = 14;     // (Serigrafia D3)
+double CO2;
+double TVOC;
+double TEMP;
+double HR;
 
-// Definición de variables
-
-double CO2_acu
-double TVOC_acu
-double temp_acu
-double hum_acu
-
-double CO2
-double TVOC
-double temp
-double hum
-
-int n
 
 Adafruit_CCS811 ccs;
 DHT dht(DHTPin, DHTTYPE);
 
+// -------------- Inicialización / Parametros WIFI -------------------
+#define WIFI_SSID "wohntextilien"
+#define WIFI_PASSWORD "*"
+// -------------------------------------------------------------------
+
+// -------------- Sincronizacion NTP ---------------------------------
+#define TZ_INFO "CET-1CEST,M3.5.0,M10.5.0/3"
+// Servidores NTP para sincronización.
+// Para la sincronización mas rápida https://www.pool.ntp.org/zone/
+#define NTP_SERVER1  "1.es.pool.ntp.org"
+#define NTP_SERVER2  "1.europe.pool.ntp.org"
+//--------------------------------------------------------------------
+
+// -------------- Incializacion / Parametros INFLUX ------------------
+#define INFLUXDB_URL "http://ataoyo-gcp.ddns.net:8086"
+#define INFLUXDB_DB_NAME "temphum"
+#define INFLUXDB_USER ""
+#define INFLUXDB_PASS ""
+#define WRITE_PRECISION WritePrecision::S
+#define MAX_BATCH_SIZE 1  // Numero de puntos que se escriben de una vez
+#define WRITE_BUFFER_SIZE 50 //Número máximo de puntos en el buffer
+                             
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_DB_NAME);
+//--------------------------------------------------------------------
+
+void config_WIFI(void)
+{
+
+
+ /************************************************************************/
+ /* Inicialización WIFI*/
+ /************************************************************************/
+
+ Serial.println("-----------------------------------------------------");
+ Serial.print("Inicializando WIFI");
+ Serial.println("-----------------------------------------------------");
+
+ WiFi.begin(WIFI_SSID, WIFI_PASSWORD);      // Conectar WIFI
+
+ Serial.println("*****************************************************");
+ Serial.print("WIFI Inicialización conectando a: ");
+ Serial.print(WIFI_SSID); Serial.println(" ...");
+
+  int i = 0;
+
+  // Esperando la conexión **********************************************
+ 
+  while (WiFi.status() != WL_CONNECTED) { 
+    delay(1000);
+    Serial.print(++i); Serial.print(' ');
+  }
+
+
+ Serial.println("Conexion establecida!");
+ Serial.print(" Direccion IP:  ");
+ Serial.println(WiFi.localIP());  // Dirección IP asignada al sensor
+ Serial.println("*****************************************************");
+
+}
+void config_TIME(void)
+ {
+   /************************************************************************/
+  /* Sincronizacion  NTP*/
+  /************************************************************************/
+  Serial.println("Sincronización NTP.");
+  Serial.println("");
+  timeSync(TZ_INFO, NTP_SERVER1, NTP_SERVER2);
+  Serial.println("*****************************************************");
+
+ }
+
+ void config_INFLUX(void)
+ {
+  /************************************************************************/
+  /* Inicializacion parametros Influx*/
+  /************************************************************************/
+  client.setConnectionParamsV1(INFLUXDB_URL, INFLUXDB_DB_NAME, INFLUXDB_USER, INFLUXDB_PASS);
+  client.setWriteOptions(WriteOptions().writePrecision(WRITE_PRECISION).batchSize(MAX_BATCH_SIZE).bufferSize(WRITE_BUFFER_SIZE));
+  Serial.println("Conexión InfluxDB realizada");
+  Serial.println("*****************************************************");
+
+ }
+
+ void InFlux_Write(void)
+{
+
+  time_t tnow;
+
+  Point pointDevice("ambientales");
+
+  // Establecemos tiempo para registro actual ------------
+  tnow=time(nullptr);
+  pointDevice.setTime(tnow);
+  // -----------------------------------------------------
+
+  // Medidas conexion WIFI -------------------------------
+  pointDevice.clearFields(); //Limpiamos registros para seguir añadiendo
+  pointDevice.setTime(tnow);
+  pointDevice.addField("CO2", CO2);
+  pointDevice.addField("TVOC", TVOC);
+  pointDevice.addField("TEMP", TEMP);
+  pointDevice.addField("HR", HR);
+
+  // -----------------------------------------------------
+  client.writePoint(pointDevice); //Registramos en dB
+}
+
 void setup() {
   Serial.begin(115200);
+
+  config_WIFI();     // Incialización WIFI (En Setup porque solo se hace una vez)
+  config_TIME();     // Sincronizacion NTP
+  config_INFLUX();   // Conexión con la base dedatos
 
   Serial.println("CCS811 test");
 
@@ -64,9 +151,11 @@ void loop() {
   if(ccs.available()){
     if(!ccs.readData()){
       Serial.print("CO2: ");
-      Serial.print(ccs.geteCO2());
-      Serial.print("ppm, TVOC: ");
-      Serial.println(ccs.getTVOC());
+      CO2 = ccs.geteCO2();
+      Serial.print(CO2);
+      TVOC = ccs.getTVOC();
+      Serial.print("ppm \t TVOC: ");
+      Serial.println(TVOC);
     }
     else{
       Serial.println("ERROR!");
@@ -74,20 +163,21 @@ void loop() {
     }
   }
 
-  float h = dht.readHumidity();
-  float t = dht.readTemperature();
+  HR = dht.readHumidity();
+  TEMP = dht.readTemperature();
 
-  if (isnan(h) || isnan(t)) {
+  if (isnan(HR) || isnan(TEMP)) {
       Serial.println("Fallo de Sensor!");
       return;
   }
 
 
   Serial.print("Humedad: ");
-  Serial.print(h);
+  Serial.print(HR);
   Serial.print(" %\t");
   Serial.print("Temperatura: ");
-  Serial.print(t);
+  Serial.print(TEMP);
   Serial.println(" *C ");
-  delay(1000);
+  InFlux_Write();
+  delay(5000);
 }
